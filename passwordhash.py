a50190fe04e434ce70f6b02027e281a896dbb81b
Dave Russell Jr <dave@createazure.com>
2015-12-27 23:40:43 +0000

    Create Python password hasher
    This commit introduces the main file, the actual hasher. Fairly simple
    as to how it works, but essentially the script uses crypt.crypt() to
    create the password hash.
    
    If you're using Python 3 (which you should be, cause Buhman says so),
    then you can use crypt.mksalt() to make the salt. This actually has that
    commented out, because the systems I'm using uses Python 2.7 by default.
    However, mksalt is probably more betterer than os.urandom().
    
    That being said, there is a cryptographically secure alternative for
    Python 2.7, which we're using. The drawback is you have to import two
    more modules with this alternative (os and binascii). We use
    os.urandom() for Python 2.7ers.
    
    Signed-off-by: Dave Russell Jr <dave@createazure.com>

---
+++ passwordhash.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# Password Hashing Module for Linux
+# Author: Dave Russell Jr (drussell393)
+
+from getpass import getpass
+import crypt
+
+# If you like Python 2, please to be importing.
+import os
+import binascii
+
+password = getpass('Enter your desired password, Harry: ')
+passwordConfirm = getpass('Confirm your password: ')
+
+if (password == passwordConfirm):
+    # Python 2 alternative, os.urandom()
+    passwordHash = crypt.crypt(password, '$6$' + binascii.hexlify(os.urandom(4)))
+
+    # Python 3 likes my crypt (mksalt doesn't work in Python 2)
+    #passwordHash = crypt.crypt(password, crypt.mksalt(crypt.METHOD_SHA512))
+
+    print('You\'re a wizard, Harry: ' + passwordHash)
+else:
+    print('Dobby has heard of your greatness, sir. But of your goodness, Dobby never knew.')
+    print('Your confirmation password didn\'t match, Oh Great One.')
