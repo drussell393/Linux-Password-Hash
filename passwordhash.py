614a5de89a5746e95dc14f371d001c5a9056f646
Dave Russell Jr <dave@createazure.com>
2020-11-29 21:41:45 -0500

    Make changes to default to Python3
    Since we have undoubtedly moved to Python3 for the most part within the community, this should be Python3 by default. We make Python2 users work harder now.

--- passwordhash.py
+++ passwordhash.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Password Hashing Module for Linux
 # Author: Dave Russell Jr (drussell393)
 
@@ -6,18 +6,18 @@ from getpass import getpass
 import crypt
 
 # If you like Python 2, please to be importing.
-import os
-import binascii
+#import os
+#import binascii
 
 password = getpass('Enter your desired password, Harry: ')
 passwordConfirm = getpass('Confirm your password: ')
 
 if (password == passwordConfirm):
     # Python 2 alternative, os.urandom()
-    passwordHash = crypt.crypt(password, '$6$' + binascii.hexlify(os.urandom(4)))
+    #passwordHash = crypt.crypt(password, '$6$' + binascii.hexlify(os.urandom(4)))
 
     # Python 3 likes my crypt (mksalt doesn't work in Python 2)
-    #passwordHash = crypt.crypt(password, crypt.mksalt(crypt.METHOD_SHA512))
+    passwordHash = crypt.crypt(password, crypt.mksalt(crypt.METHOD_SHA512))
 
     print('You\'re a wizard, Harry: ' + passwordHash)
 else:
